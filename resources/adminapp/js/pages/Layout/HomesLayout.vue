5ffe596022e01902cf15fd4f07b8fd5f55d8d2e2
Vanxay588 <Vanxay588@gmail.com>
2021-07-10 16:56:52 +0700

    Update route

--- resources/adminapp/js/pages/Layout/HomesLayout.vue
+++ resources/adminapp/js/pages/Layout/HomesLayout.vue
@@ -1,83 +1,8 @@
 <template>
-  <div class="wrapper" :class="{ 'nav-open': $sidebar.showSidebar }">
-    <side-bar :sidebarLinks="sidebarLinks">
-      <mobile-menu slot="content"></mobile-menu>
-    </side-bar>
 
     <div class="main-panel">
       <top-navbar></top-navbar> <!-- Languages -->
     </div>
-  </div>
 </template>
 
 <script>
-import TopNavbar from './TopNavbar.vue'
-import MobileMenu from './MobileMenu.vue'
-
-export default {
-  components: {
-    TopNavbar,
-    MobileMenu
-  },
-  data() {
-    return {
-      sidebarLinks: [
-        {
-          title: 'cruds.home.title',
-          icon: 'table_view',
-          path: { name: 'homes.index' }
-        }, 
-        {
-          title: 'cruds.blog.title',
-          icon: 'table_view',
-          path: { name: 'blogs.index' }
-        },
-        {
-          title: 'cruds.annoucement.title',
-          icon: 'table_view',
-          path: { name: 'annoucements.index' }
-        },
-        {
-          title: 'cruds.policyManagement.title',
-          icon: 'table_view',
-          path: { name: 'policy_management' },
-          children: [
-            {
-              title: 'cruds.policyCategory.title',
-              icon: 'table_view',
-              path: { name: 'policy_categories.index' }
-            },
-            {
-              title: 'cruds.policy.title',
-              icon: 'table_view',
-              path: { name: 'policies.index' }
-            }
-          ]
-        },
-        {
-          title: 'cruds.downloadManagement.title',
-          icon: 'table_view',
-          path: { name: 'download_management' },
-          children: [
-            {
-              title: 'cruds.downloadCategory.title',
-              icon: 'table_view',
-              path: { name: 'download_categories.index' }
-            },
-            {
-              title: 'cruds.download.title',
-              icon: 'table_view',
-              path: { name: 'downloads.index' }
-            }
-          ]
-        },
-        {
-          title: 'cruds.aboutUs.title',
-          icon: 'table_view',
-          path: { name: 'aboutuses.index' }
-        } 
-      ]
-    }
-  }
-}
-</script>
