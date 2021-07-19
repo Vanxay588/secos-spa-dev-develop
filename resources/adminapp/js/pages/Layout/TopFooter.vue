e8ed10501f33e75c67fb71e96d9b4f27213b4fbc
Vanxay588 <Vanxay588@gmail.com>
2021-07-19 12:52:51 +0700

    update topFooter

--- resources/adminapp/js/pages/Layout/TopFooter.vue
+++ resources/adminapp/js/pages/Layout/TopFooter.vue
@@ -1,36 +1,49 @@
 <template>
-<div >
+<div>
+  <div class="">
     <ul class="nav justify-content-center bg-success">
-       <li class="nav-item" >
-         <a class="nav-link " href="#homes">{{ $t('cruds.home.title') }}</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#blog">{{ $t('cruds.blog.title') }}</a>
-      </li>
-       <li class="nav-item">
-        <a class="nav-link" href="#Annoucements">{{ $t('cruds.annoucement.title') }}</a>
-      </li>
-      <li class="nav-item">
-         <a class="nav-link" href="#policy">{{ $t('cruds.policy.title') }}</a>
-       </li>
-       <li class="nav-item">
-         <a class="nav-link" href="#download"> {{ $t('cruds.download.title') }}</a>
-      </li>
-      <li class="nav-item">
-      <a class="nav-link" href="#aboutUs">{{ $t('cruds.aboutUs.title') }}</a>
-      </li>
+  <li class="nav-item">
+    <a class="nav-link " href="#homes">{{ $t('cruds.home.title') }}</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#blog">{{ $t('cruds.blog.title') }}</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#Annoucements">{{ $t('cruds.annoucement.title') }}</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#policy">{{ $t('cruds.policy.title') }}</a>
+  </li>
+   <li class="nav-item">
+    <a class="nav-link" href="#download"> {{ $t('cruds.download.title') }}</a>
+  </li>
+   <li class="nav-item">
+    <a class="nav-link" href="#aboutUs">{{ $t('cruds.aboutUs.title') }}</a>
+  </li>
   </ul>
-    <p>2021 - {{ new Date().getFullYear() }}&nbsp;&nbsp; &copy;&nbsp;{{ $t('global.allRightsReserved') }}&nbsp;&nbsp; Made With &nbsp;&nbsp;{{ $t('panel.site_title') }}</p>
-    <hr>
-</div>
+  <hr>
+  </div>
+  <p>
+   2021 - {{ new Date().getFullYear() }} &copy;  {{ $t('global.allRightsReserved') }} Made with  {{ $t('panel.site_title') }}
+  </p>
+  
+      </div>
+ 
+
+
 </template>
 <style scoped>
-  
- a {
+a {
         color: aliceblue;
       }
-     
-ul li {
+hr {
+          margin-top: auto;
+          margin-bottom: 1rem;
+          border: 0;
+          border-top: 5px solid darkslateblue;
+      }
+ul li{
+    
     display: block;
     list-style-type: disc;
     margin-block-start: 1em;
@@ -39,21 +52,13 @@ ul li {
     margin-inline-end: 0px;
     padding-inline-start: 40px;
     }
-  
- hr {
-      
-          margin-top: auto;
-          margin-bottom: 1rem;
-          border: 0;
-          border-top: 5px solid darkslateblue;
-      } 
- p   {    
-        position:absolute;
-        bottom:0px;
-        background-color:#1e1e1e;
-        width:100%;
-        text-align:center;
-        color: aliceblue;  
-        padding: 10px;
-      }
+p{
+    position: absolute;
+    bottom: 0px;
+    background-color: #1e1e1e;
+    width: 100%;
+    text-align: center;
+    padding: 10px;
+    color: aliceblue;
+     }
 </style>
